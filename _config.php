1a21b5f2d93c4fcec7a5c38c0f311c40fde8bc9b
Howard Grigg <howard@gri.gg>
2013-08-27 21:53:41 +1000

    Initial
    Initial commit

---
+++ _config.php
@@ -0,0 +1,10 @@
+<?php
+ 
+global $project;
+$project = 'library';
+ 
+global $database;
+$database = 'library-dev';
+ 
+// Use _ss_environment.php file for configuration
+require_once("conf/ConfigureFromEnv.php");
---
+++ templates/Page.ss
@@ -0,0 +1,11 @@
+<html>
+<head>
+<% base_tag %>
+$MetaTags
+</head>
+<body>
+Hello Library
+$Layout
+$Form
+</body>
+</html>
